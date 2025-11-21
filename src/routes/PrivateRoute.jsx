f85b26b0c04da4c3a02cc3f64ab1ee538dddb2f3
Malik Ubaid Awan <malikubaidawan14@gmail.com>
2025-11-21 19:13:48 +0500

    Remove comment in PrivateRoute component
    Removed comment about blocking access based on user role.

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,12 +1,9 @@
-// src/routes/PrivateRoute.jsx
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 const PrivateRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
-
-  // if no user or role not admin, block access
   if (!user || user.role !== "admin") {
     return <Navigate to="/" replace />;
   }
