1918d0d4fd99f875670c80c315558c7717459d7f
Malik Ubaid Awan <malikubaidawan14@gmail.com>
2025-11-30 16:13:28 +0500

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext.jsx";
 
 const PrivateRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
-  if (!user || user.role !== "admin") {
-    return <Navigate to="/" replace />;
-  }
+
+  if (!user) return <Navigate to="/login" replace />;
 
   return children;
 };
